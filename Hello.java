56a8fa8045271f31c83d10faab36b73098c2aae7
D_U <divya.bhosle@lntinfotech.com>
2019-11-02 03:43:43 -0700

    message

--- Hello.java
+++ Hello.java
@@ -3,4 +3,9 @@ public class Hello
     public static void main(String args[])
     String name="utkarsha";
     System.out.println("Hello"+name);
+
+     public void hello()
+   {
+       System.out.println("Hiiiiiiiii");
+   }
 }
